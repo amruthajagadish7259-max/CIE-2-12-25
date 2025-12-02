ba590cdb02d1713ad3fa202446bb4b3ad05f0e50
amruthajagadish7259-max <amruthajagadish7259@gmail.com>
2025-12-02 11:42:56 +0530

    Update test_student.py

--- test_student.py
+++ test_student.py
@@ -1,9 +1,9 @@
 from student import student_details
 def test_student_details():
     expected_output=(
-    "Student ID:01FE24BCA011\n"
-    "Student Name:Amrutha\n"
-    "Course Enrolled:DevOps\n"
-    "Academic Year:2025"
+        "Student ID:01FE24BCA011\n"
+        "Student Name:Amrutha\n"
+        "Course Enrolled:DevOps\n"
+        "Academic Year:2025"
     )
     assert product_details("01FE24BCA011","Amrutha","DevOps",2024)==expected_output
