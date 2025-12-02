90e1836b6de026c27f533b84eea174dcd6f3577f
amruthajagadish7259-max <amruthajagadish7259@gmail.com>
2025-12-02 11:47:43 +0530

    Update student.py

--- student.py
+++ student.py
@@ -1,13 +1,13 @@
 def student_details(s_id,s_name,course,academic_year):
    result=(
-    f"Student ID:{s_id}\n"
-    f"Student Name:{s_name}\n"
-    f"Course Enrolled:{course}\n"
-    f"Academic Year:{academic_year}"
-    )
+      f"Student ID:{s_id}\n"
+      f"Student Name:{s_name}\n"
+      f"Course Enrolled:{course}\n"
+      f"Academic Year:{academic_year}"
+   )
    return result
-
-if __name__=="__main__":
+   
+   if __name__=="__main__":
    #sample output(you can change)
    s_id="01FF24BCA011"
    s_name="Amrutha"
